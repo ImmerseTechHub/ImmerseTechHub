f8d04ce034aa981a4cb5423465f45bac69c9437d
JacinemiUA <131624010+JacinemiUA@users.noreply.github.com>
2023-07-20 01:46:26 +0200

    solving conflict

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom'
-<<<<<<< HEAD
-import Routes from './applications/Routes';
-import Footer from './components/Footer/Footer';
-=======
 import Router from './applications/Router';
->>>>>>> dev
+import Footer from './components/Footer/Footer';
 
 function App() {
     return (
         <BrowserRouter>
-<<<<<<< HEAD
-            <Routes/>
-            <Footer/>
-=======
             <Router/>
->>>>>>> dev
+            <Footer/>
         </BrowserRouter> 
     )   
 }
