e3934d3a696eff6bf75c8adb2a2b294d3dbc118a
JacinemiUA <131624010+JacinemiUA@users.noreply.github.com>
2023-07-20 01:33:49 +0200

    Creación del archivo Routes.jsx

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom'
+import Routes from './applications/Routes';
 
 function App() {
-
+    return (
+        <BrowserRouter>
+            <Routes/>
+        </BrowserRouter> 
+    )   
 }
 
 export default App;
